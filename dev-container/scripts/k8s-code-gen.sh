cd38426f12aa3cc74b95d3bb3b31638271b4e95b
Per Pettersson <Per.Pettersson@gmail.com>
2020-11-22 09:14:58 -0500

    k8s code gen

---
+++ dev-container/scripts/k8s-code-gen.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+REPO=github.com/pellepedro/operator
+MODULE=operator
+APIS_DIR="${REPO}/pkg/apis/config"
+VERSION="v1"
+APIS_VERSION_DIR="${APIS_DIR}/${VERSION}"
+OUTPUT_DIR="${REPO}/${MODULE}/pkg/client"
+CLIENTSET_DIR="${OUTPUT_DIR}/clientset"
+LISTERS_DIR="${OUTPUT_DIR}/listers"
+INFORMERS_DIR="${OUTPUT_DIR}/informers"
+
+echo Generating deepcopy
+deepcopy-gen --input-dirs ${APIS_VERSION_DIR} \
+	-O zz_generated.deepcopy --bounding-dirs ${APIS_DIR}
+
+echo Generating clientset
+client-gen --clientset-name versioned \
+	--input-base '' --input ${APIS_VERSION_DIR} \
+	--clientset-path ${CLIENTSET_DIR}
+
+echo Generating lister
+lister-gen --input-dirs ${APIS_VERSION_DIR} \
+	--output-package ${LISTERS_DIR}
+
+echo Generating informer
+informer-gen --input-dirs ${APIS_VERSION_DIR} \
+	--versioned-clientset-package "${CLIENTSET_DIR}/versioned" \
+	--listers-package ${LISTERS_DIR} \
+	--output-package ${INFORMERS_DIR}
+
