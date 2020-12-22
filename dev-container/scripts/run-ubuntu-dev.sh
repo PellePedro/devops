d06a5e888b58c10fa1a6bc4bfdf6b283c60c48e6
Kin Wong <kiwo@kaloom.com>
2020-12-22 08:43:14 -0500

    updated scripts for containers - to clean up container after exiting - include .gitconfig in the container

--- dev-container/scripts/run-ubuntu-dev.sh
+++ dev-container/scripts/run-ubuntu-dev.sh
@@ -3,9 +3,10 @@
 PUBLIC_UBUNTU_CONTAINER=docker.io/pellepedro/ubuntu-dev:latest
 
 printf "\033[32m\xE2\x9c\x93 Running Ubuntu Development Container \033[0m\n"
-sudo podman run -it --privileged \
+sudo podman run -it --rm --privileged \
     -v ${HOME}/.netrc:/home/devops/.netrc:z \
     -v ${HOME}/.ssh:/home/devops/.ssh:z \
+    -v ${HOME}/.gitconfig:/home/devops/.gitconfig:z \
     -v ${HOME}:/home/devops/host:rw \
     -v ${HOME}/go/pkg/mod:/home/devops/go/pkg/mod:z \
     ${PUBLIC_UBUNTU_CONTAINER} -name udev
