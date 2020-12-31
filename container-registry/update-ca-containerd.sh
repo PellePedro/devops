df4b70dc0178ad7765309cc0bfa6b1acd9a42ccf
Per Pettersson <Per.Pettersson@gmail.com>
2020-12-31 15:22:04 -0500

    Updated /etc/containerd/config.toml with registry credentials

--- container-registry/update-ca-containerd.sh
+++ container-registry/update-ca-containerd.sh
@@ -2,7 +2,8 @@
 
 # https://github.com/lxc/lxc-ci/blob/master/bin/test-lxd-snapd
 for container in $(lxc list --fast | tail -n+3 | grep "^| " | cut -d' ' -f2); do
-  lxc file push auth/registry.crt "${container}"/etc/ssl/certs/
-  lxc exec "${container}" -- update-ca-certificates
+  echo "${container}"
+  lxc file push config.toml "${container}"/etc/containerd/config.toml
+  lxc exec "${container}" -- systemctl restart containerd
 done
 
