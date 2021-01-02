9f66e2fc785dbdc422512042865480b309cfcd36
Per Pettersson <Per.Pettersson@gmail.com>
2021-01-02 15:03:24 -0500

    Added java 15 installation on Ubuntu

---
+++ dev-container/scripts/install-java15-ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:linuxuprising/java
+sudo apt update
+sudo apt install -y oracle-java15-installer
---
+++ dev-container/scripts/uninstall-java15-ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo add-apt-repository --remove ppa:linuxuprising/java
+sudo apt-get remove oracle-java15-installer
